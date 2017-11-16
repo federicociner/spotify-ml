58f4b827aef737eb5cf5724d3c7831053cbb7d20
Federico Ciner <federico.ciner@gmail.com>
2017-11-16 17:05:15 +1100

    Added generate_features.py, restructured directory, added visualisation notebook

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# spotify-ml
-Testing different machine learning models in Python to classify "good" vs "bad" songs based on features provided by the Spotify web API. Credits to Nick Behrens for putting together the original tutorial at https://towardsdatascience.com/making-your-own-discover-weekly-f1ac7546fedb.
+# Machine Learning with Spotify
+
+Testing different machine learning models in Python to classify "good" vs "bad" songs based on features provided by the Spotify web API.
+
+Credits to Nick Behrens for putting together the original tutorial at https://towardsdatascience.com/making-your-own-discover-weekly-f1ac7546fedb.
+
+See below for a breakdown of the project directory:
+* _data_ - contains the base feature datasets generated through the generate_features module in src/features
+* _models_ - contains cached/pre-trained models
+* _notebooks_ - contains data exploration and visualistion Jupyter Notebooks
+* _src_
+    1. features - source code for extracting JSON data via Spotify API and generate feature datasets
+    1. models - contains
--- src/features/generate_features.py
+++ src/features/generate_features.py
@@ -2,28 +2,81 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import os
 import numpy as np
 import pandas as pd
-from dotenv import load_env, read_file
-
+from dotenvy import load_env, read_file
 import spotipy
-import spotipy.util as util
-sp = spotipy.Spotify()
 
 
 def access_spotify(envfile_path):
+    sp = spotipy.Spotify()
     load_env(read_file(envfile_path))
-    cid = os.environ.get('SPOTIPY_CLIENT_ID')
-    secret = os.environ.get('SPOTIPY_CLIENT_SECRET')
-    redirect = os.environ.get('SPOTIPY_REDIRECT_URI')
-    username = os.environ.get('SPOTIPY_USERNAME')
+    cid = os.environ.get("SPOTIPY_CLIENT_ID")
+    secret = os.environ.get("SPOTIPY_CLIENT_SECRET")
+    username = os.environ.get("SPOTIPY_USERNAME")
 
     client_credentials_manager = SpotifyClientCredentials(
         client_id=cid, client_secret=secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    token = client_credentials_manager.get_access_token()
 
-    scope = 'user-library-read playlist-read-private'
-    token = util.prompt_for_user_token(username, scope)
+    if token:
+        sp = spotipy.Spotify(auth=token)
+    else:
+        print("Can't get token for", username)
+
+    return sp
 
 
 def get_song_ids(sp, username, p_playlist_id, n_playlist_id):
-    p_playlist =
-    n_playlist =
+    p_playlist = sp.user_playlist(username, p_playlist_id)
+    n_playlist = sp.user_playlist(username, n_playlist_id)
+
+    p_tracks = p_playlist["tracks"]
+    p_songs = p_tracks["items"]
+    while p_tracks["next"]:
+        p_tracks = sp.next(p_tracks)
+        [p_songs.append(item) for item in p_tracks["items"]]
+
+    n_tracks = n_playlist["tracks"]
+    n_songs = n_tracks["items"]
+    while n_tracks["next"]:
+        n_tracks = sp.next(n_tracks)
+        [n_songs.append(item) for item in n_tracks["items"]]
+
+    p_ids = [p_songs[i]["track"]["id"] for i in range(0, len(p_songs))]
+    n_ids = [n_songs[i]["track"]["id"] for i in range(0, len(n_songs))]
+
+    return (p_ids, n_ids)
+
+
+def get_features(sp, p_ids, n_ids):
+    features = []
+
+    # get JSON extract of features and label positive and negative classes
+    for i in range(0, len(p_ids), 100):
+        audio_features = sp.audio_features(p_ids[i:i + 100])
+        for track in audio_features:
+            features.append(track)
+            features[-1]['class'] = 1
+
+    for i in range(0, len(n_ids), 100):
+        audio_features = sp.audio_features(n_ids[i:i + 100])
+        for track in audio_features:
+            features.append(track)
+            features[-1]['class'] = 0
+
+    # convert to data frame remove irrelevant features
+    df = pd.DataFrame(features)
+    non_features = ['analysis_url', 'id', 'track_href', 'type', 'uri']
+    df.drop(labels=non_features, axis=1, inplace=True)
+
+    # move class column to the left of the dataframe
+    col_class = df['class']
+    df.drop(labels=['class'], axis=1, inplace=True)
+    df.insert(0, 'class', col_class)
+
+    return df
+
+
+def save_features(df, filename):
+    data_dir = os.path.join(os.getcwd(), os.pardir, 'data', filename)
+    df.to_csv(path_or_buf=data_dir, sep=",", header=True, index=False)
