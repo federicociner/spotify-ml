f4b5b42e18c2a3f196664333ff3b1fab07ab9427
FedericoCiner <federico.ciner@gmail.com>
2017-11-22 07:36:42 +1100

    Added BayesSearchCV

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -4,10 +4,6 @@ from sklearn.metrics import average_precision_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import hamming_loss
-from sklearn.model_selection import GridSearchCV
-from xgboost import XGBClassifier
-
-import pickle
 
 
 def model_report(y_pred, y_true):
@@ -24,43 +20,3 @@ def model_report(y_pred, y_true):
     report += clf_report
 
     return report
-
-
-def generate_param_grid():
-    param_grid = {
-        'n_estimators': [300, 500, 900, 1200, 1500],
-        'max_depth': [4, 8, 10, 12, 24],
-        'learning_rate': [0.01, 0.05, 0.1, 0.2],
-        'subsample': [0.6, 0.7, 0.8, 0.9]
-    }
-
-    return param_grid
-
-
-def train_model(X_train, y_train, scoring='roc_auc', n_jobs=2, cv=4):
-    clf_initial = XGBClassifier(n_jobs=n_jobs)  # instantiate classifier
-    param_grid = generate_param_grid()  # get parameters to tune
-    grid = GridSearchCV(clf_initial, cv=cv, n_jobs=n_jobs,
-                        param_grid=param_grid, scoring=scoring)
-    grid.fit(X_train, y_train)
-
-    # get best estimator
-    best_clf = grid.best_estimator_
-
-    # get the best parameters
-    best_params = grid.best_params_
-
-    return {"best_params": best_params, "best_estimator": best_clf}
-
-
-def save_model(model_object, filepath):
-    with open(filepath, 'wb+') as model_file:
-        pickler = pickle.Pickler(model_file)
-        pickler.dump(model_object)
-
-
-def load_model(filepath):
-    with open(filepath, 'rb+') as model_file:
-        model_object = pickle.load(model_file)
-
-    return model_object
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,1 +1,45 @@
+from sklearn.model_selection import GridSearchCV
+from skopt import BayesSearchCV
+from xgboost import XGBClassifier
+import pickle
 
+
+def generate_param_grid():
+    param_grid = {
+        'n_estimators': [300, 500, 900, 1200, 1500],
+        'max_depth': [4, 8, 10, 12, 14, 16],
+        'learning_rate': [0.01, 0.05, 0.1, 0.15, 0.2],
+        'subsample': [0.6, 0.65, 0.7, 0.75, 0.8, 0.85],
+        'colsample_bytree': [0.6, 0.7, 0.8, 0.9, 1.0]
+    }
+
+    return param_grid
+
+
+def train_model(X_train, y_train, scoring='roc_auc', n_jobs=2, cv=3):
+    clf_initial = XGBClassifier()  # instantiate classifier
+    param_grid = generate_param_grid()  # get parameters to tune
+    grid = GridSearchCV(clf_initial, cv=cv, n_jobs=n_jobs,
+                        param_grid=param_grid, scoring=scoring)
+    grid.fit(X_train, y_train)
+
+    # get best estimator
+    best_clf = grid.best_estimator_
+
+    # get the best parameters
+    best_params = grid.best_params_
+
+    return {"best_params": best_params, "best_estimator": best_clf}
+
+
+def save_model(model_object, filepath):
+    with open(filepath, 'wb+') as model_file:
+        pickler = pickle.Pickler(model_file)
+        pickler.dump(model_object)
+
+
+def load_model(filepath):
+    with open(filepath, 'rb+') as model_file:
+        model_object = pickle.load(model_file)
+
+    return model_object
