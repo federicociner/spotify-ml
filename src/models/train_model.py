73ec8eb912abb94a6d4081f30bb01431bde974e5
FedericoCiner <federico.ciner@gmail.com>
2017-11-27 07:32:59 +1100

    Saved first round of grid search

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,11 +5,12 @@ import pickle
 
 def generate_param_grid():
     param_grid = {
-        'n_estimators': [100, 300, 500, 700, 900, 1100, 1300, 1500],
-        'max_depth': [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-        'learning_rate': [0.005, 0.01, 0.05, 0.1, 0.15, 0.2],
-        'subsample': [0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95],
-        'colsample_bytree': [0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 1.0]
+        'n_estimators': [100, 300, 500, 700, 900],
+        'max_depth': [4, 5, 6, 7, 8, 9, 10, 12],
+        'learning_rate': [0.5, 0.1, 0.15, 0.2],
+        'subsample': [0.6, 0.7, 0.8, 0.9, 1.0],
+        'gamma': [0, 0.1, 0.2, 0.3],
+        'reg_lambda': [0.001, 0.01, 0.1, 0.5, 1.0]
     }
 
     return param_grid
